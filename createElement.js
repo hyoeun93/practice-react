04d227cbba3d5b81efbc913abb4c19a62c47eff2
HyoEun Lee <hyoeun93@gmail.com>
2020-03-24 23:44:37 -0400

    first commit

---
+++ createElement.js
@@ -0,0 +1,6 @@
+//old React version of creating Element
+const element = React.createElement(
+    'h1',
+    {className: "greeting"},
+    'Hello,World'
+)
---
+++ createEvent.js
@@ -0,0 +1,10 @@
+class Event extends React.Component({
+    event(evt) {
+
+    },
+    render(){
+        return(
+            <div onClick={this.event}>Click Me</div>
+        )
+    }
+})
